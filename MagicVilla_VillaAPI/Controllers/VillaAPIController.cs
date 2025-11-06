a577910af293baba9e2613c5746e80d5f2ffdd27
Ahmed Samir <ahmedsemido14@gmail.com>
2025-11-07 00:13:24 +0200

    Implement GetVilla endpoint with static data using VillaStore and DTO pattern

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]   
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(villa => villa.Id == id);
+        }
+    }
+}
